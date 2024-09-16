0648fbf522b876de354aaf3600011062912abeeb
Madonna Magdy Moussa <madonnamagdymoussa@gmail.com>
2024-09-16 16:03:45 +0300

    Add files via upload

---
+++ DMA_MemoryScatterGatherMode/HC-05_UART/Debug/subdir_vars.mk
@@ -0,0 +1,121 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c123gh6pm.cmd 
+
+C_UPPER_SRCS += \
+../GPTM.C 
+
+C_SRCS += \
+../DMA.c \
+../DMA_config.c \
+../GPIO.c \
+../GPIO_config.c \
+../GPTM_config.c \
+../HC-05.c \
+../NVIC.c \
+../SystemControl.c \
+../SystemControl_Config.c \
+../SystickTimer.c \
+../UART.c \
+../UART_config.c \
+../main.c \
+../tm4c123gh6pm_startup_ccs.c 
+
+C_UPPER_DEPS += \
+./GPTM.d 
+
+C_DEPS += \
+./DMA.d \
+./DMA_config.d \
+./GPIO.d \
+./GPIO_config.d \
+./GPTM_config.d \
+./HC-05.d \
+./NVIC.d \
+./SystemControl.d \
+./SystemControl_Config.d \
+./SystickTimer.d \
+./UART.d \
+./UART_config.d \
+./main.d \
+./tm4c123gh6pm_startup_ccs.d 
+
+OBJS += \
+./DMA.obj \
+./DMA_config.obj \
+./GPIO.obj \
+./GPIO_config.obj \
+./GPTM.obj \
+./GPTM_config.obj \
+./HC-05.obj \
+./NVIC.obj \
+./SystemControl.obj \
+./SystemControl_Config.obj \
+./SystickTimer.obj \
+./UART.obj \
+./UART_config.obj \
+./main.obj \
+./tm4c123gh6pm_startup_ccs.obj 
+
+OBJS__QUOTED += \
+"DMA.obj" \
+"DMA_config.obj" \
+"GPIO.obj" \
+"GPIO_config.obj" \
+"GPTM.obj" \
+"GPTM_config.obj" \
+"HC-05.obj" \
+"NVIC.obj" \
+"SystemControl.obj" \
+"SystemControl_Config.obj" \
+"SystickTimer.obj" \
+"UART.obj" \
+"UART_config.obj" \
+"main.obj" \
+"tm4c123gh6pm_startup_ccs.obj" 
+
+C_DEPS__QUOTED += \
+"DMA.d" \
+"DMA_config.d" \
+"GPIO.d" \
+"GPIO_config.d" \
+"GPTM_config.d" \
+"HC-05.d" \
+"NVIC.d" \
+"SystemControl.d" \
+"SystemControl_Config.d" \
+"SystickTimer.d" \
+"UART.d" \
+"UART_config.d" \
+"main.d" \
+"tm4c123gh6pm_startup_ccs.d" 
+
+C_UPPER_DEPS__QUOTED += \
+"GPTM.d" 
+
+C_SRCS__QUOTED += \
+"../DMA.c" \
+"../DMA_config.c" \
+"../GPIO.c" \
+"../GPIO_config.c" \
+"../GPTM_config.c" \
+"../HC-05.c" \
+"../NVIC.c" \
+"../SystemControl.c" \
+"../SystemControl_Config.c" \
+"../SystickTimer.c" \
+"../UART.c" \
+"../UART_config.c" \
+"../main.c" \
+"../tm4c123gh6pm_startup_ccs.c" 
+
+C_UPPER_SRCS__QUOTED += \
+"../GPTM.C" 
+
+
