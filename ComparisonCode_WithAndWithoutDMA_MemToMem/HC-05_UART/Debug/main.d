8e19999686899e05f48838a4fddc9c57254d2bc1
Madonna Magdy Moussa <madonnamagdymoussa@gmail.com>
2024-09-18 21:48:00 +0300

    Add files via upload

---
+++ ComparisonCode_WithAndWithoutDMA_MemToMem/HC-05_UART/Debug/main.d
@@ -0,0 +1,66 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: ../HC-05.h
+main.obj: ../lstd.h
+main.obj: ../GPIO.h
+main.obj: ../gpio_config.h
+main.obj: ../GPTM_config.h
+main.obj: ../GPTM.h
+main.obj: ../DMA.h
+main.obj: ../DMA_config.h
+main.obj: ../UART.h
+main.obj: ../UART_config.h
+main.obj: ../NVIC.h
+main.obj: ../NVIC_config.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdlib.h
+
+../main.c:
+
+../HC-05.h:
+
+../lstd.h:
+
+../GPIO.h:
+
+../gpio_config.h:
+
+../GPTM_config.h:
+
+../GPTM.h:
+
+../DMA.h:
+
+../DMA_config.h:
+
+../UART.h:
+
+../UART_config.h:
+
+../NVIC.h:
+
+../NVIC_config.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdlib.h:
+
