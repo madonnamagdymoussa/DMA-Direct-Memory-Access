754c93bb0f6976174f401d5ae5bc4cc03841bba5
Madonna Magdy Moussa <madonnamagdymoussa@gmail.com>
2024-09-25 01:26:07 +0300

    Add files via upload

---
+++ TwoTiva_OneTransmitPeriphToPher_OneRecievePingPong/ReceiveDMA_WorkSpace/HC-05_UART/Debug/HC-05.d
@@ -0,0 +1,24 @@
+# FIXED
+
+HC-05.obj: ../HC-05.c
+HC-05.obj: ../UART.h
+HC-05.obj: ../UART_config.h
+HC-05.obj: ../lstd.h
+HC-05.obj: ../GPIO_config.h
+HC-05.obj: ../DMA.h
+HC-05.obj: ../DMA_config.h
+
+../HC-05.c:
+
+../UART.h:
+
+../UART_config.h:
+
+../lstd.h:
+
+../GPIO_config.h:
+
+../DMA.h:
+
+../DMA_config.h:
+
